905ac1fa01f8d3362f010b1198df2915897018dd
kkkakon <k.a.konnova@mail.ru>
2020-09-19 12:36:02 +0400

    ЛР 2.1 Добавлен класс LnFunction

---
+++ src/main/java/ru/ssau/tk/chernyshev_konnova/functions/LnFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.chernyshev_konnova.functions;
+
+public class LnFunction implements MathFunction {
+
+    public double apply(double x) {
+        return java.lang.Math.log(x);
+    }
+}

00a22d531cd167f18a948b2e778eee0549342c5c
kkkakon <k.a.konnova@mail.ru>
2020-09-20 14:56:54 +0400

    ЛР 2.1 Добавлен конструктор класса CompositeFunction

--- src/main/java/ru/ssau/tk/chernyshev_konnova/functions/CompositeFunction.java
+++ src/main/java/ru/ssau/tk/chernyshev_konnova/functions/CompositeFunction.java
@@ -1,8 +1,18 @@
 package ru.ssau.tk.chernyshev_konnova.functions;
 
 public class CompositeFunction implements MathFunction {
+    private MathFunction firstFunction;
+    private MathFunction secondFunction;
+
+    public CompositeFunction(MathFunction f1, MathFunction f2){
+        this.firstFunction = f1;
+        this.secondFunction = f2;
+
+    }
+
     @Override
     public double apply(double x) {
+
         return 0;
     }
 }

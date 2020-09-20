b7697142a10ab33093e08fcb899734376f437db3
kkkakon <k.a.konnova@mail.ru>
2020-09-20 14:47:10 +0400

    ЛР 2.1 Создан класс CompositeFunction

---
+++ src/main/java/ru/ssau/tk/chernyshev_konnova/functions/CompositeFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.chernyshev_konnova.functions;
+
+public class CompositeFunction implements MathFunction {
+    @Override
+    public double apply(double x) {
+        return 0;
+    }
+}

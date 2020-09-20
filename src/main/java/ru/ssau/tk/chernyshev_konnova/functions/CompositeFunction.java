2b1f394753f633ab7c4f915d8a640f5fe9772294
kkkakon <k.a.konnova@mail.ru>
2020-09-20 15:01:54 +0400

    ЛР 2.1 Добавлена реализация метода apply класса CompositeFunction

--- src/main/java/ru/ssau/tk/chernyshev_konnova/functions/CompositeFunction.java
+++ src/main/java/ru/ssau/tk/chernyshev_konnova/functions/CompositeFunction.java
@@ -4,15 +4,13 @@ public class CompositeFunction implements MathFunction {
     private MathFunction firstFunction;
     private MathFunction secondFunction;
 
-    public CompositeFunction(MathFunction f1, MathFunction f2){
+    public CompositeFunction(MathFunction f1, MathFunction f2) {
         this.firstFunction = f1;
         this.secondFunction = f2;
-
     }
 
     @Override
     public double apply(double x) {
-
-        return 0;
+        return secondFunction.apply(firstFunction.apply(x));
     }
 }

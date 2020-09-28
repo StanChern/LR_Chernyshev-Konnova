c9da0933eea70a8b7779f7269fcb9c036e6aade3
kkkakon <k.a.konnova@mail.ru>
2020-09-28 20:17:35 +0400

    ЛР 2.2 Создан класс LinkedListTabulatedFunction

---
+++ src/main/java/ru/ssau/tk/chernyshev_konnova/functions/LinkedListTabulatedFunction.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.chernyshev_konnova.functions;
+
+public class LinkedListTabulatedFunction {
+
+    protected class Node {
+        double x;
+        double y;
+        Node next;
+        Node prev;
+    }
+
+    private Node head;
+
+    private void addNode(double x, double y) {
+        //TODO: добавление узла в конец списка
+
+    }
+
+    LinkedListTabulatedFunction(double[] xValues, double[] yValues) {
+        //TODO:конструктор с 2 параметрами
+    }
+
+    LinkedListTabulatedFunction(MathFunction source, double xFrom, double xTo, int count) {
+        //TODO: конструктор с 4 параметрами
+    }
+}
--- src/main/java/ru/ssau/tk/chernyshev_konnova/functions/Node.java
+++
@@ -1,8 +0,0 @@
-package ru.ssau.tk.chernyshev_konnova.functions;
-
-public class Node {
-    double x;
-    double y;
-    Node next;
-    Node prev;
-}

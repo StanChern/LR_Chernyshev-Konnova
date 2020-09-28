96d576d9dd9ed84807cb395d408cf1b01213d8fc
Chernyshev <Chernyshev.st17@gmail.com>
2020-09-28 21:14:11 +0400

    ЛР 2.2 в классе AbstractTabulatedFunction реализован метод apply()

--- src/main/java/ru/ssau/tk/chernyshev_konnova/functions/AbstractTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/chernyshev_konnova/functions/AbstractTabulatedFunction.java
@@ -18,4 +18,20 @@ public abstract class AbstractTabulatedFunction implements TabulatedFunction {
     protected abstract double interpolate(double x, int floorIndex);
 
     protected abstract double interpolate(double x, double leftX, double rightX, double leftY, double rightY);
+
+    public double apply(double x) {
+        if (x < leftBound()) {
+            return (extrapolateLeft(x));
+        } else {
+            if (x > rightBound()) {
+                return (extrapolateRight(x));
+            } else {
+                if (indexOfX(x) != -1) {
+                    return (getY(indexOfX(x)));
+                } else {
+                    return (interpolate(x, floorIndexOfX(x)));
+                }
+            }
+        }
+    }
 }

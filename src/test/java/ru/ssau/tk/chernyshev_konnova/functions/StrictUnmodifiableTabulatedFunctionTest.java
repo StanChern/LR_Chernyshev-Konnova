3b174a9471efa957f57bc80d2f7bd17aa0d05623
Chernyshev <Chernyshev.st17@gmail.com>
2020-10-17 10:32:23 +0400

    ЛР 3.1 Написаны тесты для двойной обертки. Задание 3.1* выполнено.

--- src/test/java/ru/ssau/tk/chernyshev_konnova/functions/StrictUnmodifiableTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/chernyshev_konnova/functions/StrictUnmodifiableTabulatedFunctionTest.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 
 import static org.testng.Assert.*;
 
-public class UnmodifiableTabulatedFunctionTest {
+public class StrictUnmodifiableTabulatedFunctionTest {
 
     private final double[] xValues = new double[]{1, 2, 3, 4, 5};
     private final double[] yValues = new double[]{10, 20, 30, 40, 50};
@@ -15,27 +15,35 @@ public class UnmodifiableTabulatedFunctionTest {
         return new ArrayTabulatedFunction(xValues, yValues);
     }
 
+    private StrictTabulatedFunction getStrictArrays() {
+        return new StrictTabulatedFunction(getDefinedThroughArrays());
+    }
+
     private LinkedListTabulatedFunction getListOfArray() {
         return new LinkedListTabulatedFunction(yValues, xValues);
     }
 
+    private UnmodifiableTabulatedFunction getUnmodifiableList() {
+        return new UnmodifiableTabulatedFunction(getListOfArray());
+    }
+
     @Test
     public void testGetCount() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertEquals(testArrayFunction.getCount(), 5);
 
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         assertEquals(testListFunction.getCount(), 5);
     }
 
     @Test
     public void testGetX() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertEquals(testArrayFunction.getX(0), 1);
         assertEquals(testArrayFunction.getX(1), 2);
         assertEquals(testArrayFunction.getX(2), 3);
 
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getUnmodifiableList());
         assertEquals(testListFunction.getX(0), 10);
         assertEquals(testListFunction.getX(1), 20);
         assertEquals(testListFunction.getX(2), 30);
@@ -43,12 +51,12 @@ public class UnmodifiableTabulatedFunctionTest {
 
     @Test
     public void testGetY() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertEquals(testArrayFunction.getY(0), 10);
         assertEquals(testArrayFunction.getY(1), 20);
         assertEquals(testArrayFunction.getY(2), 30);
 
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         assertEquals(testListFunction.getY(0), 1);
         assertEquals(testListFunction.getY(1), 2);
         assertEquals(testListFunction.getY(2), 3);
@@ -56,23 +64,23 @@ public class UnmodifiableTabulatedFunctionTest {
 
     @Test
     public void testSetY() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertThrows(UnsupportedOperationException.class, () -> testArrayFunction.setY(0, 0));
         assertThrows(UnsupportedOperationException.class, () -> testArrayFunction.setY(1, 0));
 
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         assertThrows(UnsupportedOperationException.class, () -> testListFunction.setY(0, 0));
         assertThrows(UnsupportedOperationException.class, () -> testListFunction.setY(10, 0));
     }
 
     @Test
     public void testIndexOfX() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertEquals(testArrayFunction.indexOfX(1), 0);
         assertEquals(testArrayFunction.indexOfX(2), 1);
         assertEquals(testArrayFunction.indexOfX(3), 2);
 
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         assertEquals(testListFunction.indexOfX(10), 0);
         assertEquals(testListFunction.indexOfX(20), 1);
         assertEquals(testListFunction.indexOfX(30), 2);
@@ -80,12 +88,12 @@ public class UnmodifiableTabulatedFunctionTest {
 
     @Test
     public void testIndexOfY() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertEquals(testArrayFunction.indexOfY(10), 0);
         assertEquals(testArrayFunction.indexOfY(20), 1);
         assertEquals(testArrayFunction.indexOfY(30), 2);
 
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         assertEquals(testListFunction.indexOfY(1), 0);
         assertEquals(testListFunction.indexOfY(2), 1);
         assertEquals(testListFunction.indexOfY(3), 2);
@@ -93,25 +101,25 @@ public class UnmodifiableTabulatedFunctionTest {
 
     @Test
     public void testLeftBound() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertEquals(testArrayFunction.leftBound(), 1);
 
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         assertEquals(testListFunction.leftBound(), 10);
     }
 
     @Test
     public void testRightBound() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertEquals(testArrayFunction.rightBound(), 5);
 
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         assertEquals(testListFunction.rightBound(), 50);
     }
 
     @Test
     public void testIterator() {
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         Iterator<Point> myIterator = testListFunction.iterator();
         int k = 0;
         for (Point myPoint : testListFunction) {
@@ -123,14 +131,16 @@ public class UnmodifiableTabulatedFunctionTest {
 
     @Test
     public void testApply() {
-        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getDefinedThroughArrays());
+        UnmodifiableTabulatedFunction testArrayFunction = new UnmodifiableTabulatedFunction(getStrictArrays());
         assertEquals(testArrayFunction.apply(1), 10);
         assertEquals(testArrayFunction.apply(2), 20);
-        assertEquals(testArrayFunction.apply(2.5), 25);
-
-        UnmodifiableTabulatedFunction testListFunction = new UnmodifiableTabulatedFunction(getListOfArray());
+        assertEquals(testArrayFunction.apply(3), 30);
+        StrictTabulatedFunction testListFunction = new StrictTabulatedFunction(getUnmodifiableList());
         assertEquals(testListFunction.apply(10), 1);
         assertEquals(testListFunction.apply(20), 2);
-        assertEquals(testListFunction.apply(25), 2.5);
+        assertEquals(testListFunction.apply(30), 3);
+
+        assertThrows(UnsupportedOperationException.class, () -> testArrayFunction.apply(-2));
+        assertThrows(UnsupportedOperationException.class, () -> testListFunction.apply(-2));
     }
 }
